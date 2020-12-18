e521bb47552db3d15050a59478a60579a83c55f5
tasawernawaz <tasawer.nawaz@arbisoft.com>
2020-12-18 17:21:22 +0500

    latest code

--- src/App.js
+++ src/App.js
@@ -1,70 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-import Game from './Game'
-
-
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.checkAnswer = this.checkAnswer.bind(this);
-    }
-
-    getrandomValue = () => Math.floor(Math.random() * 100);
-
-    makeNewQuestionArray = () => {
-        const value1 = this.getrandomValue();
-        const value2 = this.getrandomValue();
-        const value3 = this.getrandomValue();
-        const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-        return [value1, value2, value3, proposedAnswer];
-    }
-
-    newQuestionArray = this.makeNewQuestionArray();
-
-    state = {
-        value1: this.newQuestionArray[0],
-        value2: this.newQuestionArray[1],
-        value3: this.newQuestionArray[2],
-        proposedAnswer: this.newQuestionArray[3],
-        numQuestions: 0,
-        numCorrect: 0
-    }
-
-    checkAnswer = (event) => {
-        const answer = event.target.name;
-        const newQuestionArray = this.makeNewQuestionArray()
-        const actualAnswer = this.state.value1 + this.state.value2 + this.state.value3;
-
-        this.setState((currentState) => ({
-            numCorrect: ((answer === "true" &&  currentState.proposedAnswer === actualAnswer) ||
-                (answer === "false" &&  currentState.proposedAnswer !== actualAnswer)) ? currentState.numCorrect + 1 : currentState.numCorrect,
-            numQuestions: currentState.numQuestions + 1,
-            value1: newQuestionArray[0],
-            value2: newQuestionArray[1],
-            value3: newQuestionArray[2],
-            proposedAnswer: newQuestionArray[3]
-        })
-      )
-    }
-
+class Pure extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      update: props.toggle,
+    };
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="game">
-          <h2>Mental Math</h2>
-          <Game checkAnswer={this.checkAnswer}/>
-          <p className="text">
-            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
-          </p>
-        </div>
+      <strong>
+        <span style={{ color: 'mediumseagreen' }}>Pure: </span>
+        {new Date().getSeconds().toString()}
+      </strong>
+    );
+  }
+}
+
+const Stateless = props => (
+  <strong>
+    <span style={{ color: 'orange' }}>Stateless: </span>
+    {new Date().getSeconds().toString()}
+  </strong>
+);
+
+class Normal1 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      update: props.toggle,
+    };
+  }
+
+  render() {
+    return (
+      <strong>
+        <span style={{ color: 'dodgerblue' }}>Normal1: </span>
+        {new Date().getSeconds().toString()}
+      </strong>
+    );
+  }
+}
+
+class Normal2 extends React.Component {
+  state = {
+    update: this.props.toggle,
+  };
+
+  render() {
+    return (
+      <strong>
+        <span style={{ color: 'green' }}>Normal2: </span>
+        {new Date().getSeconds().toString()}
+      </strong>
+    );
+  }
+}
+
+class Normal3 extends React.Component {
+  shouldComponentUpdate() {
+    return false
+  }
+
+  render() {
+    return (
+      <strong>
+        <span style={{ color: 'red' }}>Normal3: </span>
+        {new Date().getSeconds().toString()}
+      </strong>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = { toggle: true };
+
+  componentDidMount() {
+    setInterval(() => {
+      // this.setState({ toggle: true });
+      this.setState({ toggle: !this.state.toggle })
+    }, 10);
+  }
+
+  render() {
+    const { toggle } = this.state;
+    return (
+      <div>
+        <Pure toggle={toggle} />
+        <br />
+        <Stateless toggle={toggle} />
+        <br />
+        <Normal1 toggle={toggle} />
+        <br />
+        <Normal2 toggle={toggle} />
+        <br />
+        <Normal3 />
+        <br />
       </div>
     );
   }
