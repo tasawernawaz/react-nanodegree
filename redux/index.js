d7bef6e04e78084743ed2b412669c7e2bbbc8b5c
tasawernawaz <tasawer.nawaz@arbisoft.com>
2020-12-28 20:35:08 +0500

    feat: add app reducer and goals

--- redux/index.js
+++ redux/index.js
@@ -1,6 +1,6 @@
 // library code
 function createStore (reducer) {
-    let state
+    let state = {}
     let listners = []
 
     const getState = () => {
@@ -30,24 +30,74 @@ function createStore (reducer) {
 
 // app code
 function todos(state=[], action) {
-    if (action.type === "ADD_TODO") {
-        state = state.concat(action.todo)
+
+    switch(action.type) {
+        case "ADD_TODO":
+            return state.concat(action.todo)
+        case "REMOVE_TODO":
+            return state.filter(s => s.id !== action.id)
+        case "TOGGLE_TODO":
+            return state.map(todo => todo.id !== action.id ? todo :
+                Object.assign({}, todo, {complete: ! todo.complete}))
+        default:
+            return state
     }
-    return state
 }
 
+function goals(state=[], action) {
 
-const store = createStore(todos)
+    switch(action.type) {
+        case "ADD_GOAL":
+            return state.concat(action.goal)
+        case "REMOVE_GOAL":
+            return state.filter(s => s.id !== action.id)
+        default:
+            return state
+    }
+}
 
-store.subsucribe(() => console.log("The current state of the sote is ", store.getState()))
+function app(state, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
 
-const action = {
+const store = createStore(app)
+
+store.subsucribe(() => console.log("The current state of the store is ", store.getState()))
+
+let action = {
     type: "ADD_TODO",
     todo: {
         id: 1,
-        name: "Reading a Book",
+        name: "Complete redux",
+        complete: false
+    }
+}
+
+store.dispatch(action)
+
+
+action = {
+    type: "ADD_TODO",
+    todo: {
+        id: 1,
+        name: "watch video",
         complete: true
     }
 }
 
 store.dispatch(action)
+
+
+const goal = {
+    type: "ADD_GOAL",
+    goal: {
+        id: 0,
+        name: "Lose weight"
+    }
+}
+
+store.dispatch(action)
+store.dispatch(goal)
